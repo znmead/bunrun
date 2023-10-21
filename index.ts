7565ba4b6c44ea1e8256026b70024ff8fad4e1e0
Zach Mead <zachary.n.mead@gmail.com>
2023-10-21 03:54:27 -0500

    figlet working, added dev script

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
+import figlet from "figlet";
+
+
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response('Hello WWIII!')
+        const body = figlet.textSync('Pranaam!', {
+            font: 'Chunky',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 80,
+            whitespaceBreak: true
+        }) + '\n' + req.url
+        return new Response(body)
     }
 }); // => Server
 
